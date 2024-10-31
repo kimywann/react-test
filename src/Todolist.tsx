de2fe2ee97606162deccf4dfa3dae926db82533a
kimywann <Kimywan10@gmail.com>
2024-11-01 00:28:36 +0900

    study(Todolist): 새로운 할 일 추가 및 체크박스를 통한 완료 상태 토글 기능 구현

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 type Todo = {
     id : number;
@@ -14,18 +15,56 @@ const TodoList : React.FC = () => {
         {id : 1, text: '공부하기', isChecked: false},
         {id : 2, text: '잠자기', isChecked: false},
         {id : 3, text: '미팅하기', isChecked: false}
-    ])
+    ]);
+
+    const [newTodo, setNewTodo] = useState<string>('');
+
+    const handleCheckedChange = (itemId : number) => {
+        setTodos((prevItems) =>
+            prevItems.map((item) => 
+                item.id === itemId ? { ...item, isChecked : !item.isChecked } : item
+            )
+        )
+    }
+
+    const addTodo = () => {
+        if (newTodo.trim() !== '') {
+            setTodos([...todos, { id : Date.now(), text : newTodo, isChecked : false }])
+            setNewTodo('');
+        }
+    }
 
     return (
         <div>
             <h1>{title}</h1>
             <p></p>
             <div className="container">
+                <div>
+                    <input type="text"
+                        placeholder='할 일 입력'
+                        style={{marginRight : '10px', writingMode : 'horizontal-tb'}}
+                        onChange={(e)=> setNewTodo(e.target.value)}
+                    />
+                    <Button variant="warning" onClick={addTodo}>추가</Button>
+                </div>
+                <p></p>
                 <div className="board">
                 <ul>
                     {
                         todos.map((todo, index) => (
-                            <li key={index}>{todo.text}</li>
+                            <li key={todo.id}>
+                                <input type="checkbox"
+                                onChange={()=>{
+                                    handleCheckedChange(todo.id);
+                                }}></input>
+                                <span>
+                                    {
+                                        todo.isChecked ? 
+                                        <del>{todo.text}</del>
+                                        : <span>{todo.text}</span>
+                                    }
+                                </span>
+                            </li>
                         ))
                     }
                 </ul>
